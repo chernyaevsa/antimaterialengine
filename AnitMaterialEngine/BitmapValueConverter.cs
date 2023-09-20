c36eb66d5f618ffb6ec03a8ba8929e287b201157
chernyaevsa2 <csa2@czsa.ru>
2023-09-20 13:47:34 +0500

    Add explosion button

---
+++ AnitMaterialEngine/BitmapValueConverter.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnitMaterialEngine
+{
+	public class BitmapValueConverter : IValueConverter
+	{
+		public static BitmapValueConverter Instance = new BitmapValueConverter();
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is string && targetType == typeof(IBitmap))
+			{
+				var uri = new Uri((string)value, UriKind.RelativeOrAbsolute);
+				var scheme = uri.IsAbsoluteUri ? uri.Scheme : "file";
+
+				switch (scheme)
+				{
+					case "file":
+						return new Bitmap((string)value);
+
+					default:
+						var assets = AvaloniaLocator.Current.GetService<IAssetLoader>();
+						return new Bitmap(assets.Open(uri));
+				}
+			}
+
+			throw new NotSupportedException();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotSupportedException();
+		}
+	}
+}
